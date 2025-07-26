0dec9324b23f1519e54aa44bde54a7b3151ae98a
salam mohamed <91378482+SalamMohamed11@users.noreply.github.com>
2025-07-26 03:51:35 +0300

    Delete eduplatform-PROJECT directory

--- eduplatform-PROJECT/src/main/java/com/platform/eduplatform/repository/QuestionRepo.java
+++
@@ -1,7 +0,0 @@
-package com.platform.eduplatform.repository;
-
-import com.platform.eduplatform.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepo extends JpaRepository<Question, Integer> {
-}
--- eduplatform-PROJECT/src/main/java/com/platform/eduplatform/repository/ResultRepo.java
+++
@@ -1,7 +0,0 @@
-package com.platform.eduplatform.repository;
-
-import com.platform.eduplatform.model.Result;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultRepo extends JpaRepository<Result, Integer> {
-}
